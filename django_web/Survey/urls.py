cc85469d3a0098953d817cb3680dd4e2474d5b11
Rituraj <rsshbn@umsystem.edu>
2022-04-10 03:37:21 -0500

    linked

--- django_web/Survey/urls.py
+++ django_web/Survey/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home,name='home')
+    # path('',views.indexview.as_view(),name='index_view'),
+    path('Survey/',views.survey, name="survey_view"),
+    path('Depression/',views.DepressionView.as_view(),name="depression_view"),
+    path('', views.home, name='index_view'),
 ]
--- django_web/Survey/views.py
+++ django_web/Survey/views.py
@@ -1,8 +1,37 @@
-
+from re import template
+from django.views import generic
 from django.shortcuts import render
 import pickle
 
 # Create your views here.
 
+# class indexview(generic.DetailView):
+#     template_name="index.html"
 def home(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+def getPredictions(list1):
+    model = pickle.load(open('ml_model.sav', 'rb'))
+
+    prediction = model.predict(list1)
+    
+    if prediction == 1:
+        return 'Lonely'
+    elif prediction == 2:
+        return 'Anxiety'
+    elif prediction == 3:
+        return 'Stress'
+    elif prediction == 4:
+        return 'Depression'
+    else:
+        return 'Normal'
+
+# class SurveyView(generic.DetailView):
+#     template_name="Survey.html"
+
+def survey(request):
+    return render(request,'survey.html')
+
+class DepressionView(generic.TemplateView):
+    template_name="depression.html"
+
